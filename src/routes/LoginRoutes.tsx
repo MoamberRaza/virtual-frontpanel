4d14616d29bd7567878bc7bf89e8fe783214123c
Moamber <moamber@ternio.com>
2026-01-30 03:14:28 +0500

    init.

---
+++ src/routes/LoginRoutes.tsx
@@ -0,0 +1,53 @@
+import { lazy } from 'react';
+
+// project-imports
+import AuthLayout from 'layout/Auth';
+import Loadable from 'components/Loadable';
+
+// render - login
+const AuthLogin = Loadable(lazy(() => import('pages/auth/auth2/login2')));
+const AuthRegister = Loadable(lazy(() => import('pages/auth/auth2/register2')));
+const AuthForgotPassword = Loadable(lazy(() => import('pages/auth/auth2/forgot-password2')));
+const AuthCheckMail = Loadable(lazy(() => import('pages/auth/auth2/check-mail2')));
+const AuthResetPassword = Loadable(lazy(() => import('pages/auth/auth2/reset-password2')));
+const AuthCodeVerification = Loadable(lazy(() => import('pages/auth/auth2/code-verification2')));
+
+// ==============================|| AUTH ROUTES ||============================== //
+
+const LoginRoutes = {
+  path: '/',
+  children: [
+    {
+      path: '/',
+      element: <AuthLayout />,
+      children: [
+        {
+          path: 'login',
+          element: <AuthLogin />
+        },
+        {
+          path: 'register',
+          element: <AuthRegister />
+        },
+        {
+          path: 'forgot-password',
+          element: <AuthForgotPassword />
+        },
+        {
+          path: 'check-mail',
+          element: <AuthCheckMail />
+        },
+        {
+          path: 'reset-password',
+          element: <AuthResetPassword />
+        },
+        {
+          path: 'code-verification',
+          element: <AuthCodeVerification />
+        }
+      ]
+    }
+  ]
+};
+
+export default LoginRoutes;
